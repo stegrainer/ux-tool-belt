4b7db464b0da72f73d7e4913608be5ba2a815200
Ste Grainer <ste.grainer@ecsgrainer16.local>
2016-09-09 18:35:02 -0400

    Refining scripts

--- -/js/scripts.js
+++ -/js/scripts.js
@@ -1,28 +1,16 @@
 window.addEventListener('hashchange', function() {
-    var viewID = location.hash.slice(1);
-    window.scrollTop = 0;
-		var actT = document.querySelectorAll("nav a[href='#"+viewID+"']");
-		var allT = document.querySelectorAll("nav a");
-		[].forEach.call(allT, function(el) {
-			el.classList.remove("active");
-		});
-		[].forEach.call(actT, function(el) {
-			el.classList.add("active");
-		});
+	setAcTab();
 });
 
-var tabs = document.querySelectorAll("nav a"),
-		i = tabs.length;
-
-while(i--) {
-	links[i].onclick = function() {
-		var hash = this.href;
-		if(history.pushState) {
-			history.pushState(null, null, hash);
-		} else {
-			location.hash = hash;
+function setAcTab() {
+	var viewID = location.hash.slice(1);
+    
+	var actT = document.querySelectorAll("nav a[href='#"+viewID+"']");
+	var allT = document.querySelectorAll("nav a");
+	[].forEach.call(allT, function(el) {
+		el.classList.remove("active");
+		if(el.getAttribute('href') == '#'+viewID) {
+			el.classList.add("active");
 		}
-		
-		return false;
-	}
+	});
 }
