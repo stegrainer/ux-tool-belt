ea6635b10f87b37fff61efb5dfb930c049efa87b
Ste Grainer <ste.grainer@ecsgrainer16.corp.advancestores.com>
2016-09-13 16:09:30 -0400

    Updated modal and CSS

--- -/inc/card.php
+++ -/inc/card.php
@@ -1,7 +1,7 @@
 <?
-	if($_REQUEST) {
-		if($_REQUEST['tool']) {
-			$id = $_REQUEST['tool'];
+	if($_GET) {
+		if($_GET['tool']) {
+			$id = $_GET['tool'];
 			$tool = getTool($id, $tools);
 		} else {
 			$tool = false;
@@ -21,13 +21,21 @@
 			<h4>Learn more:</h4>
 			<ul>
 			<? foreach($tool->links as $link): ?>
-				<li><a href="<?= $link->url ?>"><?= $link->title ?></a></li>
+				<li><a href="<?= $link->url ?>" tabindex="1"><?= $link->title ?></a></li>
 			<? endforeach; ?>
 			</ul>
 			<? endif; ?>
 			<form action="./" method="get">
 				<input type="hidden" name="add" value="<?= $tool->index ?>" />
-				<button type="submit">Add to your tool belt</button>
+				<div class="field">
+					<label for="time">Time (in hours):</label>
+					<input type="number" name="time" value="5" min="1" max="100" step="1" id="time" tabindex="1">
+				</div>
+				<div class="field">
+					<label for="cost">Cost:</label>
+					<input type="number" name="cost" value="100" min="100" max="5000" step="100" id="cost" tabindex="1">
+				</div>
+				<button type="submit" tabindex="1">Add to your tool belt</button>
 			</form>
 		</div>
 	</div>
